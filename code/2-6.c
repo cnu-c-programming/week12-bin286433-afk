86ea6cc2b8d217244197fbdaac8d5a1724c3b3d6
bin286433-afk <bin286433@gmail.com>
2026-05-22 10:45:55 +0900

    Update 2-6.c

--- code/2-6.c
+++ code/2-6.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, const char* argv[]) {
-    if (argc < 3)
+int main(int argc, const char* argv[]){
+    if(argc < 3){
         return 0;
+    }
 
     FILE* fp = fopen(argv[1], "r");
     const char* target_str = argv[2];
 
+    char buffer[1024];
+    while(fgets(buffer, sizeof(buffer), fp)!= EOF){
+        if(strstr(buffer, target_str)!=NULL){
+            printf("%s", buffer);
+        }
+    }
 
-    
     fclose(fp);
+    return 0;
 }
-
