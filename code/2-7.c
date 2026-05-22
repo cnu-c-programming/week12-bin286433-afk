8e3e46efb3eec3b118b6ac09e1aaef85f71e26e0
bin286433-afk <bin286433@gmail.com>
2026-05-22 11:00:44 +0900

    Update 2-7.c

--- code/2-7.c
+++ code/2-7.c
@@ -2,26 +2,32 @@
 #include <string.h>
 #include <stdlib.h>
 
-typedef struct config {
+typedef struct config{
     char InputFileName[64];
     int Options;
     char SectionName[64];
     unsigned long long Address;
 } Config;
 
-void config_parser(Config* config_ptr) {
+void config_parser(Config* config_ptr){
+    FILE* fp = fopen("config.txt", "r");
+    fscanf(fp, "Options=%d\n", &config_ptr->Options);
+    fscanf(fp, "InputFileName=%s\n", &config_ptr->InputFileName);
+    fscanf(fp, "SectionName=%s\n", &config_ptr->SectionName);
+    fscanf(fp, "Address=%d\n", &config_ptr->Address);
+    
 
 }
 
-int main(int argc, const char* argv[]) {
+int main(int argc, const char* argv[]){
     Config config;
     config_parser(&config);
 
-    printf("config: %s %d %s %llu\n", 
-        config.InputFileName, 
+    printf("config: %s %d %s %llu\n",
+        config.InputFileName,
         config.Options,
         config.SectionName,
         config.Address);
-    return 0;
+    return 0;    
 }
 
