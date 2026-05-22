b7d2438e7e05d023d99a36b0d0f337ea3cf87f32
bin286433-afk <bin286433@gmail.com>
2026-05-22 11:42:02 +0900

    Update 2-7.c

--- code/2-7.c
+++ code/2-7.c
@@ -14,7 +14,7 @@ void config_parser(Config* config_ptr){
     fscanf(fp, "Options=%d\n", &config_ptr->Options);
     fscanf(fp, "InputFileName=%s\n", config_ptr->InputFileName);
     fscanf(fp, "SectionName=%s\n", config_ptr->SectionName);
-    fscanf(fp, "Address=%llu\n", &config_ptr->Address);
+    fscanf(fp, "Address=%lli\n", &config_ptr->Address);
     
 
 }
