29de328e202e4e99be22a23c528c58a964aaddad
bin286433-afk <bin286433@gmail.com>
2026-05-22 10:35:52 +0900

    Update 2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 
-int main(int argc, const char* argv[]) {
-    if (argc < 2)
+int main(int argc, const char* argv[]){
+    if(argc < 2){
         return 0;
-
+    }
     FILE* fp = fopen(argv[1], "r");
 
+    if(fp==NULL) return 0;
 
+    int counts[26] = {0};
+    int ch;
+
+    while((ch=fgets(fp))!=EOF){
+        if(ch>='a'&& ch<='z'){
+            counts[ch-'a']++;
+        }
+    }
+
+    for(int i = 0; i<26; i++){
+        printf("%c: %d\n", 'a'+i, counts[i]);
+    }
 
     fclose(fp);
+    return 0;
 }
-
