334109e05759d63103642c5f0e0fcfd4dfd5c0d5
bin286433-afk <bin286433@gmail.com>
2026-05-22 11:05:34 +0900

    Update 2-7.c

--- code/2-7.c
+++ code/2-7.c
@@ -12,8 +12,8 @@ typedef struct config{
 void config_parser(Config* config_ptr){
     FILE* fp = fopen("config.txt", "r");
     fscanf(fp, "Options=%d\n", &config_ptr->Options);
-    fscanf(fp, "InputFileName=%s\n", &config_ptr->InputFileName);
-    fscanf(fp, "SectionName=%s\n", &config_ptr->SectionName);
+    fscanf(fp, "InputFileName=%s\n", config_ptr->InputFileName);
+    fscanf(fp, "SectionName=%s\n", config_ptr->SectionName);
     fscanf(fp, "Address=%d\n", &config_ptr->Address);
     
 
